9fd0cd576569d6bfc5e6deaf2c76854fd9b97022
ajandaur <anmol.jandaur@gmail.com>
2021-12-22 20:41:09 -0800

    added functional UI

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,40 +1,56 @@
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-// Write a fucntions that plays a single round of the game 
-function playRound(playerSelection, computerSelection) {
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
     let result = ""
-    // if: player beats computer
+
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-    (playerSelection == 'scissors' && computerSelection == 'paper') ||
-    (playerSelection == 'paper' && computerSelection == 'rock')) {
-
-
-        result = ('You win! ' + playerSelection + ' beats ' + computerSelection)
-    } 
-    // else if: its a tie 
-    else if (playerSelection == computerSelection) {
-        result = ('It\'s a tie. You both chose ' + playerSelection)
-    }
-    // else: computer beats player
-    else {
-        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection)
-    }
-
-    return result
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        // Runs 5 times 
-        var playerSelection = window.prompt("Enter rock, paper, or scissors").toLowerCase()
-        const computerSelection = computerPlay();
-
-        result = playRound(playerSelection, computerSelection)
-        console.log(result)
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
         
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
     }
 
+    document.getElementById('result').innerHTML = result
+    return
 }
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
